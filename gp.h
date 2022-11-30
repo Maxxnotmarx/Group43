3415ee45ca6f09c2d37ddc06e05b36e9f6e346ff
He Ziyi <113870612+Maxxnotmarx@users.noreply.github.com>
2022-11-30 09:44:42 +0800

    Upload the header file

---
+++ gp.h
@@ -0,0 +1,22 @@
+// gp.h
+
+#ifndef GP_H
+#define GP_H
+
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+void Classic();
+void Easy( string word );
+string TargetWord( char voca, int level );
+string RandomWord( string file );
+void display_L1( string word, vector< string > output, int times, char guess_blank[], vector< char > guessed );
+void MainPage();
+//bool Search_ID(User *UserList, string Player_ID, int size);
+void Scoring();
+void display_Scoring( string word, vector< string > output, int times,char guess_blank[],vector< char > wrong_letter );
+void menu();
+
+#endif
